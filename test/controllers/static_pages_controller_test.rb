ecc897b328f9aee8aeff5e231d67b85430a02ee7
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-10-21 16:19:44 -0400

    Add a Contact static page.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,6 +13,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
   test "should get help" do
     get static_pages_help_url
     assert_response :success
