aa7d6bc13b37c9279e28ad19f91b999de65d0ec6
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-10-22 17:51:02 -0400

    Added a full_title helper function to generate page titles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Returns the full web page title with an optional per-page subtitle.
+  def full_title(page_title = '')
+    base_title = "Rating Stone"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
