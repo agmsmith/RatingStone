253371d78078a93f6cc447006f3268f3259343b3
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-10-25 15:29:08 -0400

    Validate that User name and e-mail aren't empty or too long.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name needs to be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email needs to be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "name shouldn't be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+    @user.name = "a" * 50
+    assert @user.valid?
+  end
+
+  test "email shouldn't be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+    @user.email = "a" * 243 + "@example.com"
+    assert @user.valid?
+  end
 end
