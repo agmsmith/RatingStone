7d39029f688c57500f41fb56f4ebad06c28d1bc0
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-10-27 17:10:14 -0400

    Signup form with error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else # Bad inputs, ask the user to redo the form.
+      render('new')
+    end
+  end
+
+  private
+
+  def user_params # Sanitise the inputs from the submitted form data.
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
   end
 end
