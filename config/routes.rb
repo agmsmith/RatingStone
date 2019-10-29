9e1602f16abf08c14fc15c56a0cf2560a8dacaa5
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-10-29 17:30:54 -0400

    Move unencrypted warning to Sign Up page, fix Navbar on small screens.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
