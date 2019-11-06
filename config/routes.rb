4b4f6b78bd298c8a90b85d068d021f0ddffd2b0a
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-06 14:59:26 -0500

    Minor work in progress.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
 
   resources :users
