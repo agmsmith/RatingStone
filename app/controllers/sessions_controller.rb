7822e270ca757635446f5d5bc5a4c0049a6cf775
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-09 15:31:29 -0500

    Login now works!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+    if user&.authenticate(params[:session][:password])
+      # Log the user in (save session cookie) and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # Create an error message.
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
-      render 'new'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render('new')
     end
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module SessionsHelper
+  # Logs in the given user (creates a session cookie storing the userid).
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns and caches the current logged-in user object (if any).
+  def current_user
+    if @current_user.nil? && session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    end
+  end
 end
