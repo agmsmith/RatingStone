ca18fc31c22d44c801bb94a99dc3a7f4ee926456
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-11 17:58:14 -0500

    Have a default index.html displayed when offline.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/index.html', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
