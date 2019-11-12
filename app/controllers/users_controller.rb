061f86d7bdcd95decb9ab15f24f77c5b41d83777
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-12 16:19:03 -0500

    Validate and save the results of editing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+    else
+      render('edit')
+    end
+  end
+
   private
 
   def user_params # Sanitise the inputs from the submitted form data.
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: {
+      name: "",
+      email: "foo@invalid",
+      password: "foo",
+      password_confirmation: "bar",
+    } }
+    assert_template 'users/edit'
+    assert_select 'div.alert', "The form contains 4 errors."
+    assert_select 'div#error_explanation' do
+      assert_select 'li', 4, "Should be 4 errors in our bad edit."
+    end
+  end
+end
