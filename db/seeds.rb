fa331e0d28de5f4511c9d1472d9bb5650c149223
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-13 16:42:30 -0500

    Add the admin boolean property to Users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@
 User.create!(
   name:  "Example User",
   email: "example@railstutorial.org",
-  password:              "foobar",
-  password_confirmation: "foobar")
+  password: "foobar",
+  password_confirmation: "foobar",
+  admin: true)
 
 # Generate a bunch of additional users.
 99.times do |n|
