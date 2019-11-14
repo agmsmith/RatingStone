d69fce42857fa52fbb35b8e6f538fb7cdff75de6
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-14 16:14:59 -0500

    More work on making e-mails for actvation requests.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
