e53a4feb40f84263f98d479e11afb0ba7869c560
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-15 12:22:56 -0500

    Move activation functionality into User model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
-      flash[:info] = "Please check your email to activate your account."
+      @user.send_activation_email
+      flash[:info] = "Please check your email (#{@user.email}) to activate " \
+        "your account."
       redirect_to(root_url)
     else # Bad inputs.
       render('new') # Ask the user to redo the form.
