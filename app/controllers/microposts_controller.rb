b5e074a78e858ab7e32536feb4a71b89c6c216be
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-11-21 18:02:52 -0500

    Deletion of Microposts now works.  Also fix Delete button position.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,7 @@
 
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @new_micropost = current_user.microposts.build(micropost_params)
@@ -15,11 +16,20 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    post_title = @micropost.content.truncate(50, separator: ' ')
+    @micropost.destroy
+    flash[:success] = "Micropost \"#{post_title}\" deleted."
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
