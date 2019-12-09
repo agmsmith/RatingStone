c4afe67e629f9500292ab35b897a8aa915fc5723
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-12-09 15:41:48 -0500

    Validate uploaded images by type and size.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0'
+# Add validation functionality to ActiveStorage for uploaded pictures.
+gem 'active_storage_validations', '~> 0'
 # Use Puma as the app server
 gem 'puma', '~> 3'
 # Use SCSS for stylesheets
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,13 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) } # SQL: order('created_at DESC')
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image,
+    content_type: {
+      in: %w[image/jpeg image/gif image/png],
+      message: "must be a picture format we can handle (JPEG, PNG, GIF)",
+    },
+    size: {
+      less_than: 5.megabytes,
+      message: "should be less than 5 megabytes",
+    }
 end
