deaa9440597dee10ac61bd6f7d5625dcc5a98d6d
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-12-26 14:11:04 -0500

    Added Relationship model for followers/followed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: :Relationship,
+    foreign_key: :follower_id, dependent: :destroy
 
   attr_accessor :activation_token, :remember_token, :reset_token
 
