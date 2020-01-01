ecd41bfb9a072ef51ec1e7f35f5827755e653275
Alexander G. M. Smith <agmsmith@ncf.ca>
2020-01-01 17:38:06 -0500

    AJAX for the Follow/Unfollow button.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RatingStone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Include the authenticity token in remote (AJAX) forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
