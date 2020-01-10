edcaca6cf5e4d0bec13626858c8931960887c3bd
Alexander G. M. Smith <agmsmith@ncf.ca>
2020-01-10 10:30:00 -0500

    Better activation welcome message.

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,7 +6,8 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in(user)
-      flash[:success] = "Account activated.  Welcome to Rating Stone!"
+      flash[:success] =
+        "Account activated.  Welcome to Rating Stone, #{user.name}!"
       redirect_to(user)
     else
       flash[:danger] = "Invalid activation link.  " \
