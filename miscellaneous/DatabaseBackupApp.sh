3fd368dec3f7abe8d35992491c52370f4eee61e4
Alexander G. M. Smith <agmsmith@ncf.ca>
2020-02-05 11:59:51 -0500

    Trying to get database backup and restore working.

--- miscellaneous/DatabaseBackupApp.sh
+++ miscellaneous/DatabaseBackupApp.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # AGMS20190210 Dump the database to a file in SQL format, assumes password
 # in ~/.pgpass (host:port:database:user:password, use * for host and port).
+# Presumes that the database will be empty before it is restored.
  
 umask 027
-pg_dump --verbose --clean --dbname=SomeDBName --user=SomeUser --no-password > /media/SomeFolderName/RatingStone.sql
+pg_dump --verbose --dbname=SomeDBName --user=SomeUser --no-password > /media/SomeFolderName/RatingStone.sql
 
 rm -rv /var/www/SomeWWWName/storage/va/ri/variants/
 rmdir /var/www/SomeWWWName/storage/va/ri
--- miscellaneous/DatabaseRestoreApp.sh
+++ miscellaneous/DatabaseRestoreApp.sh
@@ -3,6 +3,11 @@
 # in ~/.pgpass (host:port:database:user:password, use * for host and port).
  
 umask 027
+
+dropdb --echo --user=SomeUser --no-password SomeDBName
+createdb --echo --user=SomeUser --no-password --template=template0 --owner=SomeUser SomeDBName
 psql --dbname=SomeDBName --user=SomeUser --no-password --file /media/SomeFolderName/RatingStone.sql
+
 rsync --progress --verbose --itemize-changes --archive --delete-during --inplace --partial /media/SomeFolderName/storage /var/www/SomeWWWName/
 
+echo "Restore of database and files has been completed."
