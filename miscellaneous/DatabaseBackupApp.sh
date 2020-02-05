a7085f99c12a16cd45555c302bd0f88e2adb0e04
Alexander G. M. Smith <agmsmith@ncf.ca>
2020-02-05 13:13:24 -0500

    Finally figure out database backup & restore.

--- miscellaneous/DatabaseBackupApp.sh
+++ miscellaneous/DatabaseBackupApp.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # AGMS20190210 Dump the database to a file in SQL format, assumes password
-# in ~/.pgpass (host:port:database:user:password, use * for host and port).
-# Presumes that the database will be empty before it is restored.
+# in ~/.pgpass (host:port:database:user:password, use * for host and port
+# and database).  Presumes that the database will be empty before restoration.
  
 umask 027
-pg_dump --verbose --dbname=SomeDBName --user=SomeUser --no-password > /media/SomeFolderName/RatingStone.sql
 
+# Save the database as SQL statements.  Not using --clean due to some
+# foreign key constraints that cause trouble later during restoration.
+# Instead the restore script will delete everything first.
+pg_dump --verbose --dbname=SomeDBName --username=SomeUser --no-password > /media/SomeFolderName/SomeDBName.sql
+
+# Save the ActiveStorage files uploaded by the users, except variants (which
+# get recreated whenever somebody accesses one).
 rm -rv /var/www/SomeWWWName/storage/va/ri/variants/
 rmdir /var/www/SomeWWWName/storage/va/ri
 rmdir /var/www/SomeWWWName/storage/va
--- miscellaneous/DatabaseRestoreApp.sh
+++ miscellaneous/DatabaseRestoreApp.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 # AGMS20191209 Restore the database from a .sql text file.  Credentials
-# in ~/.pgpass (host:port:database:user:password, use * for host and port).
+# in ~/.pgpass (host:port:database:user:password, use * for host and port
+# and database too since we do things while our main database doesn't exist).
  
 umask 027
 
-dropdb --echo --user=SomeUser --no-password SomeDBName
-createdb --echo --user=SomeUser --no-password --template=template0 --owner=SomeUser SomeDBName
-psql --dbname=SomeDBName --user=SomeUser --no-password --file /media/SomeFolderName/RatingStone.sql
+# Make a totally empty database, else restore won't work well.
+dropdb --echo --username=SomeUser --no-password SomeDBName
+createdb --echo --username=SomeUser --no-password --template=template0 SomeDBName
+psql --dbname=SomeDBName --username=SomeUser --no-password --command="COMMENT ON DATABASE \"SomeDBName\" IS 'The SomeDBName reputation system database.  Used for running the SomeWWWName web site.';"
 
+# Restore the database from the saved SQL statements file.
+psql --dbname=SomeDBName --username=SomeUser --no-password --file /media/SomeFolderName/RatingStone.sql
+
+# Restore the ActiveStorage monitored files used for pictures and other media.
 rsync --progress --verbose --itemize-changes --archive --delete-during --inplace --partial /media/SomeFolderName/storage /var/www/SomeWWWName/
 
 echo "Restore of database and files has been completed."
