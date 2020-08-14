18fb80dc68e86d0b92b33d8647dc8d483da2782c
Alexander G. M. Smith <agmsmith@ncf.ca>
2020-08-14 16:51:38 -0400

    Gems updated, Rubocop now working again.

--- test/models/link_group_role_delegation_test.rb
+++ test/models/link_group_role_delegation_test.rb
@@ -6,17 +6,17 @@ class LinkGroupRoleDelegationTest < ActiveSupport::TestCase
   test "Parent should be a FullGroup and child a SubGroup" do
     lsubgroup = ledger_subgroups(:group_dogs)
     lfullgroup = ledger_full_groups(:group_all)
-    kDelegate = LinkGroupRoleDelegation.new(
-      delegate_to: lsubgroup, subgroup: lfullgroup)
-    assert_not(kDelegate.valid?, "Should check types of link endpoints.")
-    assert_equal(3, kDelegate.errors.messages.count, "Should be some errors.")
-    assert(kDelegate.errors.messages.include?(:creator))
-    assert(kDelegate.errors.messages.include?(:nonfullgroup))
-    assert(kDelegate.errors.messages.include?(:nonsubgroup))
+    kdelegate = LinkGroupRoleDelegation.new(delegate_to: lsubgroup,
+      subgroup: lfullgroup)
+    assert_not(kdelegate.valid?, "Should check types of link endpoints.")
+    assert_equal(3, kdelegate.errors.messages.count, "Should be some errors.")
+    assert(kdelegate.errors.messages.include?(:creator))
+    assert(kdelegate.errors.messages.include?(:nonfullgroup))
+    assert(kdelegate.errors.messages.include?(:nonsubgroup))
 
-    kDelegate = LinkGroupRoleDelegation.new(
-      delegate_to: lfullgroup, subgroup: lsubgroup, creator_id: 0)
-    assert(kDelegate.valid?)
-    kDelegate.save!
+    kdelegate = LinkGroupRoleDelegation.new(delegate_to: lfullgroup,
+      subgroup: lsubgroup, creator_id: 0)
+    assert(kdelegate.valid?)
+    kdelegate.save!
   end
 end
