2961f82caabde1ba099e604a750cc39ca736b1f9
Alexander G. M. Smith <agmsmith@ncf.ca>
2020-11-29 20:10:22 -0500

    Now counts regular words.

--- app/controllers/word_counter_controller.rb
+++ app/controllers/word_counter_controller.rb
@@ -2,5 +2,15 @@
 
 class WordCounterController < ApplicationController
   def update
+    @vo_script = params[:vo_script]
+    @vo_script = "Replace this text with your script..." unless @vo_script
+    words = @vo_script.downcase.split(/\s+/).sort
+    @word_count = words.length
+    @word_list = Hash.new(0)
+    words.each do |word|
+      @word_list[word] += 1
+    end
+    @vo_expanded = "Not done yet; expanded version of script ->>#{@vo_script}<<- with all the words expanded to longer words will go here."
+    render
   end
 end
