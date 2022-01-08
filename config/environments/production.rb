8bdfd12be2358e486ca3d581f1f0803efba61d91
Alexander G. M. Smith <agmsmith@ncf.ca>
2022-01-08 17:28:04 -0500

    Use new mailer settings technique for Rails 7.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Use :test for internal testing, :smtp for normal mail server.
   config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port => '587',
     :authentication => :plain,
