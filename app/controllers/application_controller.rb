2e9569b7037dcaf3b9416aab1a4ae1168282dd9d
Alexander G. M. Smith <agmsmith@ncf.ca>
2022-01-16 15:20:35 -0500

    Detect BeOS NetPositive and only remove nosniff for it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
 
+  after_action :fix_nosniff
+
+  # Only put in the X-Content-Type-Options=nosniff in the HTTP response header,
+  # when the web browser is not BeOS's NetPositive.  We took it out of the
+  # default headers, so it doesn't show up in error pages etc.  NetPositive
+  # has a bug which interprets that header as Content-Type.  Its ID string:
+  # "Mozilla/3.0 (compatible; NetPositive/2.2.1; BeOS)"
+  def fix_nosniff
+    unless request.headers["HTTP_USER_AGENT"].include?("NetPositive/")
+      response.headers["X-Content-Type-Options"] = "nosniff"
+    end
+  end
+
   private
 
   # Confirms a logged-in user.  Otherwise redirects to error page.
