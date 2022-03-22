f5233af1f00d39e5aed184ba7d305ee15795a42a
Alexander G. M. Smith <agmsmith@ncf.ca>
2022-03-22 15:50:18 -0400

    Added number of example uses to WordCounter statistics.

--- miscellaneous/WordCountUsage.sh
+++ miscellaneous/WordCountUsage.sh
@@ -7,13 +7,25 @@
 # The statistics of words counted:
 # ...]   337 word script expanded to 341 words (4 extra).
 
-awk 'BEGIN {FS=" "}
+awk 'BEGIN {
+  FS=" "
+  examples=0
+  usecount=0
+  wordtotal=0
+  extratotal=0
+}
 /] Started POST "\/server01\/wordcounter"/ {IPs[$12]++}
 /"commit"=>"Update the Count"/ {usecount++}
 /]   [0-9]+ word script expanded to [0-9]+ words \([0-9]+ extra\)\./ {
-  wordtotal+=$8
-  extrawordtotal+=substr($15,2)
+  extra=substr($15,2) # Remove the bracket before the number of extra words.
+  if (extra == 636) # Example text has 636 extra words.
+    examples++
+  else
+  {
+    wordtotal+=$8
+    extratotal+=extra
   }
+}
 END {totalposts=0
   counter=0
   for (addr in IPs) {
@@ -21,5 +33,7 @@ END {totalposts=0
     totalposts+=IPs[addr]
     counter++
   }
-  print "Unique users: " counter "  POSTs: " totalposts "  Actual uses: " usecount "  Words: " wordtotal "  Extra words: " extrawordtotal " (" extrawordtotal * 100 / wordtotal "%)"
+  if (wordtotal <= 0)
+    wordtotal=1 # Avoid divide by zero.
+  print "Unique users: " counter "  POSTs: " totalposts "  Examples: " examples "  Actual uses: " usecount "  Words: " wordtotal "  Extra words: " extratotal " (" extratotal * 100 / wordtotal "%)"
 }' < ../log/production.log | sort
