02e993675127c250af243e1c40a758038879a627
Alexander G. M. Smith <agmsmith@ncf.ca>
2022-08-12 15:36:58 -0400

    Display user's current rating points in the global page header.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,18 +18,32 @@ module ApplicationHelper
   def points_html(lobject)
     lorig = lobject.original_version # Points are stored in the original.
     result = ""
+
+    # Labels for the points have a style: 0 for ASCII words to label things
+    # like Up/Meh/Down, Reply, Quote when displaying them to this user.
+    # 1 for short ASCII like ^~v for up/meh/down.  2 for UTF font icons
+    # or emoji.
+    label_style = current_user&.fancy_labels
+    label_style = 0 if label_style.nil? || label_style < 0
+    label_style = 2 if label_style > 2
+
     if lorig.current_up_points != 0
-      result += format("^%.2f ", lorig.current_up_points)
+      result += ["Up ", "^", "👍 "][label_style] +
+        format("%.2f", lorig.current_up_points)
     end
     if lorig.current_meh_points != 0
-      result += format("~%.2f ", lorig.current_meh_points)
+      result += " / " unless result.empty?
+      result += ["Meh ", "~", "🤏 "][label_style] +
+        format("%.2f", lorig.current_meh_points)
     end
     if lorig.current_down_points != 0
-      result += format("v%.2f ", lorig.current_down_points)
+      result += " / " unless result.empty?
+      result += ["Down ", "v", "👎 "][label_style] +
+        format("%.2f", lorig.current_down_points)
     end
     result = result.strip
     result = "~" if result.empty?
-    result
+    result.html_safe
   end
 
   ##
